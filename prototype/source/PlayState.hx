6e522a2f211fd2edb90adb8dd2cce9cd3aac67c8
nightblade9 <nightbladecodes@gmail.com>
2018-01-28 19:52:19 -0500

    Fix JSON Load/trace word data

--- prototype/source/PlayState.hx
+++ prototype/source/PlayState.hx
@@ -3,17 +3,20 @@ package;
 import flixel.FlxG;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
-
+import haxe.Json;
 import helix.core.HelixState;
 import helix.core.HelixSprite;
 import helix.core.HelixText;
 import helix.data.Config;
+import openfl.Assets;
 
 class PlayState extends HelixState
 {
 	override public function create():Void
 	{
 		super.create();
+		var wordData = Json.parse(Assets.getText("assets/data/json.txt"));
+		trace(wordData);
 	}
 
 	override public function update(elapsed:Float):Void
