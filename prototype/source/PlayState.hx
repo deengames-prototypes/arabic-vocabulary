77beb6c0129f280c132d5d8ad9b1c50260aaae12
nightblade9 <nightbladecodes@gmail.com>
2018-01-30 20:36:51 -0500

    Current round: four covered cards

--- prototype/source/PlayState.hx
+++ prototype/source/PlayState.hx
@@ -6,6 +6,7 @@ import flixel.math.FlxMath;
 import flixel.math.FlxRandom;
 import flixel.ui.FlxButton;
 import haxe.Json;
+using haxesharp.collections.Linq;
 import helix.core.HelixState;
 import helix.core.HelixSprite;
 using helix.core.HelixSpriteFluentApi;
@@ -15,6 +16,8 @@ import openfl.Assets;
 
 class PlayState extends HelixState
 {
+	private static inline var PADDING:Int = 16;
+
 	private var allWords = new Array<Word>();
 	private var random = new FlxRandom();
 	
@@ -28,17 +31,28 @@ class PlayState extends HelixState
 			this.allWords.push(w);
 		}
 
-		var word = random.getObject(this.allWords);
-		var card = new Card('assets/images/${word.english}.png', word.arabic, word.english);
-		this.add(card);
-
-		card.x = 100;
-		card.y = 50;
+		this.generateAndShowRound();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+	}
+
+	private function generateAndShowRound():Void
+	{
+		var numWords:Int = Std.int(Config.get("wordsPerRound"));
+		var words = this.allWords.shuffle().take(numWords);
+		var i:Int = 0;
+
+		for (word in words)
+		{
+			var card = new Card('assets/images/${word.english}.png', word.arabic, word.english);
+			this.add(card);
+			card.x = PADDING + (i * card.width) + (i * PADDING);
+			card.y = 50;
+			i += 1;
+		}
 	}
 }
 
@@ -56,27 +70,54 @@ class Word
 
 class Card extends FlxSpriteGroup
 {
+	private static inline var CARD_WIDTH_TO_FIT:Float = 200;
 	private static inline var PADDING:Int = 8;
 	private static inline var DEFAULT_FONT_SIZE:Int = 32;
+	
+	public var cardBase:HelixSprite;
+	public var cover:HelixSprite;
+	public var image:HelixSprite;
+	public var englishText:HelixText;
+	public var arabicText:HelixText;
 
 	public function new(imageFile:String, arabic:String, english:String)
 	{
 		super();
-		
-		var cardBase = new HelixSprite("assets/images/card-base.png");
+
+		this.cardBase = new HelixSprite("assets/images/card-base.png");
 		this.add(cardBase);
 
-		var arabicText = new HelixText(PADDING, PADDING, arabic, DEFAULT_FONT_SIZE);
+		this.arabicText = new HelixText(PADDING, PADDING, arabic, DEFAULT_FONT_SIZE);
 		arabicText.x += (cardBase.width - arabicText.width)  / 2;
 		this.add(arabicText);
 
-		var image = new HelixSprite(imageFile);
+		this.image = new HelixSprite(imageFile);
 		image.move((cardBase.width - image.width) / 2, (cardBase.height - image.height) / 2);
 		this.add(image);
 
-		var englishText = new HelixText(Std.int(PADDING), Std.int(cardBase.height - PADDING), english, DEFAULT_FONT_SIZE);
+		this.englishText = new HelixText(Std.int(PADDING), Std.int(cardBase.height - PADDING), english, DEFAULT_FONT_SIZE);
 		englishText.x += (cardBase.width - englishText.width)  / 2;
 		englishText.y -= englishText.height;
 		this.add(englishText);
+
+		// Last so it goes on top
+		this.cover = new HelixSprite("assets/images/card-cover.png");
+		this.add(cover);
+
+		this.scaleToFit();
+	}
+
+	// DEBUG/TEMP: scale down images to fit four cards on-screen
+	private function scaleToFit():Void
+	{
+		this.scaleDown(this.cardBase);
+		this.scaleDown(this.cover);
+		this.scaleDown(this.image);
+	}
+
+	private function scaleDown(image:HelixSprite):Void
+	{
+		image.setGraphicSize(CARD_WIDTH_TO_FIT, 0); // Scale proportionally				
+		image.updateHitbox();
 	}
 }
