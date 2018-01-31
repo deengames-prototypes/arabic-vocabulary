b4518c8a15a7374e4f014ac4a57b239bde2de304
nightblade9 <nightbladecodes@gmail.com>
2018-01-30 21:26:04 -0500

    Show target word + English-only

--- prototype/source/PlayState.hx
+++ prototype/source/PlayState.hx
@@ -16,10 +16,13 @@ import openfl.Assets;
 
 class PlayState extends HelixState
 {
+	private static inline var TARGET_FONT_SIZE = 64;
 	private static inline var PADDING:Int = 16;
 
 	private var allWords = new Array<Word>();
+	private var targetWord:Word;
 	private var random = new FlxRandom();
+	private var targetText:HelixText;
 	
 	override public function create():Void
 	{
@@ -35,6 +38,7 @@ class PlayState extends HelixState
 		}
 
 		this.generateAndShowRound();
+		this.targetText = new HelixText(400, PADDING, this.targetWord.arabic, TARGET_FONT_SIZE);				
 	}
 
 	override public function update(elapsed:Float):Void
@@ -48,12 +52,18 @@ class PlayState extends HelixState
 		var words = this.allWords.shuffle().take(numWords);
 		var i:Int = 0;
 
+		this.targetWord = words[0];
+		if (this.targetText != null)
+		{
+			this.targetText.text = targetWord.arabic;
+		}
+
 		for (word in words)
 		{
 			var card = new Card('assets/images/${word.english}.png', word.arabic, word.english);
 			this.add(card);
 			card.x = PADDING + (i * card.width) + (i * PADDING);
-			card.y = 50;
+			card.y = 2 * TARGET_FONT_SIZE;
 			i += 1;
 		}
 	}
@@ -75,7 +85,7 @@ class Card extends FlxSpriteGroup
 {
 	private static inline var CARD_WIDTH_TO_FIT:Float = 200;
 	private static inline var PADDING:Int = 8;
-	private static inline var DEFAULT_FONT_SIZE:Int = 32;
+	public static inline var DEFAULT_FONT_SIZE:Int = 32;
 	
 	public var cardBase:HelixSprite;
 	public var cover:HelixSprite;
@@ -92,7 +102,8 @@ class Card extends FlxSpriteGroup
 
 		this.arabicText = new HelixText(PADDING, PADDING, arabic, DEFAULT_FONT_SIZE);
 		arabicText.x += (cardBase.width - arabicText.width)  / 2;
-		this.add(arabicText);
+		this.arabicText.alpha = 0;
+		this.add(arabicText);		
 
 		this.image = new HelixSprite(imageFile);
 		image.move((cardBase.width - image.width) / 2, (cardBase.height - image.height) / 2);
@@ -105,6 +116,7 @@ class Card extends FlxSpriteGroup
 
 		// Last so it goes on top
 		this.cover = new HelixSprite("assets/images/card-cover.png");
+		cover.alpha = 0;		
 		this.add(cover);
 	}
 }
