66403187dee949f8fc29d28b9ad808169c9988cf
nightblade9 <nightbladecodes@gmail.com>
2018-02-02 22:39:26 -0500

    More bug fixes.

--- FlxProject.js
+++ FlxProject.js
@@ -200,7 +200,7 @@ ApplicationMain.init = function() {
 	}
 };
 ApplicationMain.main = function() {
-	ApplicationMain.config = { build : "14", company : "Deen Games", file : "FlxProject", fps : 60, name : "FlxProject", orientation : "", packageName : "com.example.myapp", version : "0.0.1", windows : [{ antialiasing : 0, background : 0, borderless : false, depthBuffer : false, display : 0, fullscreen : false, hardware : false, height : 540, parameters : "{}", resizable : false, stencilBuffer : true, title : "FlxProject", vsync : true, width : 960, x : null, y : null}]};
+	ApplicationMain.config = { build : "16", company : "Deen Games", file : "FlxProject", fps : 60, name : "FlxProject", orientation : "", packageName : "com.example.myapp", version : "0.0.1", windows : [{ antialiasing : 0, background : 0, borderless : false, depthBuffer : false, display : 0, fullscreen : false, hardware : false, height : 540, parameters : "{}", resizable : false, stencilBuffer : true, title : "FlxProject", vsync : true, width : 960, x : null, y : null}]};
 };
 ApplicationMain.start = function() {
 	var hasMain = false;
@@ -4789,36 +4789,40 @@ PlayState.prototype = $extend(helix_core_HelixState.prototype,{
 					if(word1[0] == _gthis.targetWord) {
 						var _this = _gthis.correctSound;
 						_this.cleanup(_this.autoDestroy,true);
-						_gthis.correctSound.onComplete = (function() {
+						_gthis.correctSound.onComplete = (function(word2) {
 							return function() {
 								var key = "" + _gthis.targetWord.english + "-arabic";
 								var _this1 = _gthis.wordSounds;
 								var wordSound = __map_reserved[key] != null ? _this1.getReserved(key) : _this1.h[key];
-								wordSound.onComplete = (function() {
+								wordSound.onComplete = (function(word3) {
 									return function() {
-										var key1 = "" + _gthis.targetWord.english + "-english";
-										var _this2 = _gthis.wordSounds;
-										(__map_reserved[key1] != null ? _this2.getReserved(key1) : _this2.h[key1]).play();
+										if(word3[0] == _gthis.targetWord) {
+											var key1 = "" + _gthis.targetWord.english + "-english";
+											var _this2 = _gthis.wordSounds;
+											(__map_reserved[key1] != null ? _this2.getReserved(key1) : _this2.h[key1]).play();
+										}
 									};
-								})();
-								wordSound.play();
+								})(word2);
+								if(word2[0] == _gthis.targetWord) {
+									wordSound.play();
+								}
 							};
-						})();
+						})(word1);
 						_gthis.correctSound.play();
-						haxe_Log.trace("WIN!",{ fileName : "PlayState.hx", lineNumber : 97, className : "PlayState", methodName : "generateAndShowRound"});
+						haxe_Log.trace("WIN!",{ fileName : "PlayState.hx", lineNumber : 103, className : "PlayState", methodName : "generateAndShowRound"});
 						_gthis.tweenCards();
 					} else {
 						var _this3 = _gthis.incorrectSound;
 						_this3.cleanup(_this3.autoDestroy,true);
-						_gthis.incorrectSound.onComplete = (function(word2) {
+						_gthis.incorrectSound.onComplete = (function(word4) {
 							return function() {
-								var key2 = "" + word2[0].english + "-arabic";
+								var key2 = "" + word4[0].english + "-arabic";
 								var _this4 = _gthis.wordSounds;
 								(__map_reserved[key2] != null ? _this4.getReserved(key2) : _this4.h[key2]).play();
 							};
 						})(word1);
 						_gthis.incorrectSound.play();
-						haxe_Log.trace("NO!!",{ fileName : "PlayState.hx", lineNumber : 109, className : "PlayState", methodName : "generateAndShowRound"});
+						haxe_Log.trace("NO!!",{ fileName : "PlayState.hx", lineNumber : 115, className : "PlayState", methodName : "generateAndShowRound"});
 						_gthis.fadeCardIntoOblivion(card1[0]);
 					}
 				};
@@ -4867,7 +4871,7 @@ PlayState.prototype = $extend(helix_core_HelixState.prototype,{
 		var key = "" + this.targetWord.english + "-arabic";
 		var _this = this.wordSounds;
 		var sound = __map_reserved[key] != null ? _this.getReserved(key) : _this.h[key];
-		haxe_Log.trace("Playing " + this.targetWord.english + "-arabic",{ fileName : "PlayState.hx", lineNumber : 157, className : "PlayState", methodName : "playCurrentWord"});
+		haxe_Log.trace("Playing " + this.targetWord.english + "-arabic",{ fileName : "PlayState.hx", lineNumber : 163, className : "PlayState", methodName : "playCurrentWord"});
 		sound.cleanup(sound.autoDestroy,true);
 		sound.play();
 	}
