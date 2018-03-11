2871480eea3e07c44b72a71adaeaed9859f9e251
nightblade9 <nightbladecodes@gmail.com>
2018-03-11 18:02:01 -0400

    Refactoring: extract classes

--- code/source/LevelSelectState.hx
+++ code/source/LevelSelectState.hx
@@ -1,7 +1,5 @@
 package;
 
-import Map;
-
 import flixel.FlxG;
 import flixel.tweens.FlxTween;
 
@@ -9,14 +7,11 @@ import helix.core.HelixSprite;
 using helix.core.HelixSpriteFluentApi;
 import helix.core.HelixState;
 import helix.core.HelixText;
-import helix.data.Config;
 using haxesharp.collections.Linq;
 
-import model.GameMode;
 import model.Level;
-import model.Word;
-import utils.WordsParser;
 import utils.LevelPersister;
+import utils.LevelMaker;
 import view.Gem;
 import view.LevelButton;
 
@@ -137,62 +132,4 @@ class LevelSelectState extends HelixState
     {
         this.gemsText.text = '${currentGems}/${totalGems} gems';
     }
-}
-
-class LevelMaker
-{
-    private var LEVEL_TYPES:Array<GameMode> = [GameMode.AskInArabic, GameMode.AskInEnglish, GameMode.Mixed];
-
-    private var words:Array<Word>;
-
-    public function new() { }
-
-    public function createLevels():Array<Level>
-    {
-		this.words = WordsParser.getAllWords();
-        var newWordsPerLevel = Config.get("newWordsPerLevel");
-        var repeatWordsPerLevel = Config.get("repeatWordsPerLevel");
-        var wordsPerLevel = newWordsPerLevel + repeatWordsPerLevel;
-        
-        var numLevels = Std.int(Math.ceil(this.words.length / newWordsPerLevel));
-        trace('${this.words.length} words, ${newWordsPerLevel} per level, ${numLevels} levels');
-
-        var levels = new Array<Level>();
-
-        for (i in 0 ... numLevels)
-        {
-            var start = i * newWordsPerLevel;
-            var stop = (i + 1) * newWordsPerLevel;
-            
-            var levelWords = this.words.slice(start, stop);
-            var oldWords = new Array<Word>();
-
-            if (i == 0)
-            {
-                // First level. Just pick the words with index > start sequentially.
-                oldWords = this.words.slice(stop, stop + repeatWordsPerLevel);
-            }
-            else
-            {
-                // If it's the last level, maybe we didn't get enough words. Take more.
-                if (levelWords.length < stop - start)
-                {
-                    var delta = (stop - start - levelWords.length);
-                    repeatWordsPerLevel += delta;
-                }
-
-                // Not the first level
-                // Randomly pick from words with index < start
-                oldWords = this.words.slice(0, start).shuffle().take(repeatWordsPerLevel);
-            }
-
-            levelWords = levelWords.concat(oldWords);
-            // map [0..10] to [0..3]
-            var index = Std.int(i * this.LEVEL_TYPES.length / numLevels);
-            var levelType = this.LEVEL_TYPES[index];
-            levels.push(new Level(levelWords, levelType, i));
-        }
-
-        return levels;
-    }    
 }
---
+++ code/utils/LevelMaker.hx
@@ -0,0 +1,67 @@
+package utils;
+
+using haxesharp.collections.Linq;
+
+import helix.data.Config;
+
+import model.GameMode;
+import model.Level;
+import model.Word;
+
+class LevelMaker
+{
+    private var LEVEL_TYPES:Array<GameMode> = [GameMode.AskInArabic, GameMode.AskInEnglish, GameMode.Mixed];
+
+    private var words:Array<Word>;
+
+    public function new() { }
+
+    public function createLevels():Array<Level>
+    {
+		this.words = WordsParser.getAllWords();
+        var newWordsPerLevel = Config.get("newWordsPerLevel");
+        var repeatWordsPerLevel = Config.get("repeatWordsPerLevel");
+        var wordsPerLevel = newWordsPerLevel + repeatWordsPerLevel;
+        
+        var numLevels = Std.int(Math.ceil(this.words.length / newWordsPerLevel));
+        trace('${this.words.length} words, ${newWordsPerLevel} per level, ${numLevels} levels');
+
+        var levels = new Array<Level>();
+
+        for (i in 0 ... numLevels)
+        {
+            var start = i * newWordsPerLevel;
+            var stop = (i + 1) * newWordsPerLevel;
+            
+            var levelWords = this.words.slice(start, stop);
+            var oldWords = new Array<Word>();
+
+            if (i == 0)
+            {
+                // First level. Just pick the words with index > start sequentially.
+                oldWords = this.words.slice(stop, stop + repeatWordsPerLevel);
+            }
+            else
+            {
+                // If it's the last level, maybe we didn't get enough words. Take more.
+                if (levelWords.length < stop - start)
+                {
+                    var delta = (stop - start - levelWords.length);
+                    repeatWordsPerLevel += delta;
+                }
+
+                // Not the first level
+                // Randomly pick from words with index < start
+                oldWords = this.words.slice(0, start).shuffle().take(repeatWordsPerLevel);
+            }
+
+            levelWords = levelWords.concat(oldWords);
+            // map [0..10] to [0..3]
+            var index = Std.int(i * this.LEVEL_TYPES.length / numLevels);
+            var levelType = this.LEVEL_TYPES[index];
+            levels.push(new Level(levelWords, levelType, i));
+        }
+
+        return levels;
+    }    
+}
