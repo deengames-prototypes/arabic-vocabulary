4b09283ecb76bf07fedaf725f088d6b9a2894a8f
nightblade9 <nightbladecodes@gmail.com>
2018-03-11 17:59:14 -0400

    Refactoring: extract class

--- code/source/LevelSelectState.hx
+++ code/source/LevelSelectState.hx
@@ -18,6 +18,7 @@ import model.Word;
 import utils.WordsParser;
 import utils.LevelPersister;
 import view.Gem;
+import view.LevelButton;
 
 class LevelSelectState extends HelixState
 {   
@@ -135,39 +136,6 @@ class LevelSelectState extends HelixState
     private function updateGemsText():Void
     {
         this.gemsText.text = '${currentGems}/${totalGems} gems';
-    }
-}
-
-class LevelButton extends HelixSprite
-{
-    private static inline var FONT_SIZE:Int = 32;
-
-    private static var LEVEL_MODE_IMAGES:Map<GameMode, String> = [
-        GameMode.AskInArabic => "words-button-1",
-        GameMode.AskInEnglish => "words-button-2",
-        GameMode.Mixed => "words-button-3",
-    ];
-
-    private var text:HelixText;
-
-    public function new(levelNum:Int, level:Level, isEnabled:Bool)
-    {
-        var suffix = isEnabled ? "" : "-disabled";
-        super('assets/images/${LEVEL_MODE_IMAGES[level.levelType]}${suffix}.png');
-        this.text = new HelixText(0, 0, '${levelNum + 1}', FONT_SIZE);
-        if (isEnabled) {
-            this.onClick(function() {
-                FlxG.switchState(new PlayState(level));
-            });
-        }
-    }
-
-    public function move(x, y):Void
-    {
-        this.x = x; 
-        this.y = y;
-        this.text.x = Std.int(this.x + (this.width / 2));
-        this.text.y = Std.int(this.y + (this.height / 2));
     }
 }
 
---
+++ code/source/view/LevelButton.hx
@@ -0,0 +1,43 @@
+package view;
+
+import flixel.FlxG;
+
+import helix.core.HelixSprite;
+using helix.core.HelixSpriteFluentApi;
+import helix.core.HelixText;
+
+import model.GameMode;
+import model.Level;
+
+class LevelButton extends HelixSprite
+{
+    private static inline var FONT_SIZE:Int = 32;
+
+    private static var LEVEL_MODE_IMAGES:Map<GameMode, String> = [
+        GameMode.AskInArabic => "words-button-1",
+        GameMode.AskInEnglish => "words-button-2",
+        GameMode.Mixed => "words-button-3",
+    ];
+
+    private var text:HelixText;
+
+    public function new(levelNum:Int, level:Level, isEnabled:Bool)
+    {
+        var suffix = isEnabled ? "" : "-disabled";
+        super('assets/images/${LEVEL_MODE_IMAGES[level.levelType]}${suffix}.png');
+        this.text = new HelixText(0, 0, '${levelNum + 1}', FONT_SIZE);
+        if (isEnabled) {
+            this.onClick(function() {
+                FlxG.switchState(new PlayState(level));
+            });
+        }
+    }
+
+    public function move(x, y):Void
+    {
+        this.x = x; 
+        this.y = y;
+        this.text.x = Std.int(this.x + (this.width / 2));
+        this.text.y = Std.int(this.y + (this.height / 2));
+    }
+}
