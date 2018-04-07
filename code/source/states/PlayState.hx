c008d265f43af3200a3a5901a36e10224471b954
nightblade9 <nightbladecodes@gmail.com>
2018-04-07 15:59:32 -0400

    Extract Card view class

--- code/source/states/PlayState.hx
+++ code/source/states/PlayState.hx
@@ -1,7 +1,6 @@
 package states;
 
 import flixel.FlxG;
-import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxRandom;
 import flixel.system.FlxSound;
 import flixel.tweens.FlxTween;
@@ -18,6 +17,7 @@ import model.GameMode;
 import model.Level;
 import model.Word;
 import utils.SaveManager;
+import view.Card;
 import view.Gem;
 import view.TutorialWindow;
 
@@ -369,58 +369,5 @@ class QuestionAnswerMediator
 	public function get_answerLanguage():String
 	{
 		return this.mode == GameMode.AskInArabic ? "english" : "arabic";
-	}
-}
-
-class Card extends FlxSpriteGroup
-{
-	private static inline var CARD_WIDTH_TO_FIT:Float = 200;
-	private static inline var PADDING:Int = 8;
-	private static inline var DEFAULT_FONT_SIZE:Int = 32;
-	
-	public var cardBase:HelixSprite;
-	public var cover:HelixSprite;
-	public var image:HelixSprite;
-	public var englishText:HelixText;
-	public var arabicText:HelixText;
-	public var word:Word;
-	private var clickable:Bool = true;
-
-	public function new(imageFile:String, word:Word, mode:GameMode)
-	{
-		super();
-
-		this.word = word;
-		this.cardBase = new HelixSprite("assets/images/ui/card-base.png");		
-		this.add(cardBase);
-
-		this.arabicText = new HelixText(PADDING, PADDING, word.arabic, DEFAULT_FONT_SIZE);
-		arabicText.x += (cardBase.width - arabicText.width)  / 2;
-		this.arabicText.alpha = mode == GameMode.AskInArabic ? 0 : 1;
-		this.add(arabicText);		
-
-		this.image = new HelixSprite(imageFile);
-		image.move((cardBase.width - image.width) / 2, (cardBase.height - image.height) / 2);
-		this.add(image);
-
-		this.englishText = new HelixText(Std.int(PADDING), Std.int(cardBase.height - PADDING), word.english, DEFAULT_FONT_SIZE);
-		englishText.x += (cardBase.width - englishText.width)  / 2;
-		englishText.y -= englishText.height;
-		this.englishText.alpha = mode == GameMode.AskInArabic ? 1 : 0;
-		this.add(englishText);
-	}
-
-	public function onClick(callback:Void->Void):Void
-	{
-		this.cardBase.onClick(function() {
-			if (this.clickable) {
-				this.clickable = false;
-				callback();
-			}
-		});
-	}
-
-	public function disable() {
-		this.cardBase.onClick(function() { });
 	}
 }
